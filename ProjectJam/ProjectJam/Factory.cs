eaab793d3dcfdc911c2b9490f01e2cda5fe79535
jakob arndal <takana@exelion.dk>
2016-02-09 09:31:47 +0100

    Nu Med Factory og QualityTest

---
+++ ProjectJam/ProjectJam/Factory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Factory
+    {
+        public static Factory factory;
+
+        public static Factory GetFactory() {
+            if (factory == null)
+            {
+                factory = new Factory();
+                
+            }
+            return factory;
+        }
+        public QualityTestFactory GetQTF() {
+            return factory.GetQTF();
+        } 
+
+        //internal QualityTestFactory qtf;
+   
+       
+
+
+    }
+}
---
+++ ProjectJam/ProjectJam/QualityTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class QualityTest : IQualityTest
+    {
+
+        private int prodID;
+        private DateTime date;
+        private string qualityTestActivities, expResults, employee,comment, results;
+
+        public QualityTest(int prodID, DateTime date,
+                string qualityTestActivities, string expResults,
+                 string employee)
+        {
+            this.prodID = prodID;
+            this.date = date;
+            this.qualityTestActivities = qualityTestActivities;
+            this.expResults = expResults;
+            this.employee = employee;
+            this.results = null;
+            this.comment = null;
+
+        }
+
+        public QualityTest(int prodID, DateTime date,
+                string qualityTestActivities, string expResults,
+                 string employee, string comment, string results)
+        {
+
+           
+
+            this.prodID = prodID;
+            this.date = date;
+            this.qualityTestActivities = qualityTestActivities;
+            this.expResults = expResults;
+            this.employee = employee;
+            this.results = results;
+            this.comment = comment;
+        }
+    }
+}
