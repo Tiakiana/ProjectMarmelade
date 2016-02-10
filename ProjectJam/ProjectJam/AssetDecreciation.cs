ac0850cd025f41a8df6c724c156ae1a1e74b7eb1
Søren Hansen <soer078m@edu.eal.dk>
2016-02-10 13:29:26 +0100

    Asset namespaces changed

--- ProjectJam/ProjectJam/AssetDecreciation.cs
+++ ProjectJam/ProjectJam/AssetDecreciation.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MarmeladeAssets
+namespace Domain
 {       enum DecreciationType { LinearyMethod, BalanceMethod, AnnuityMethod};
     class AssetDecreciation
     {
