4caad5a30e9c82334fceb0a700f415dbfb980dea
Søren Hansen <soer078m@edu.eal.dk>
2016-02-10 13:02:30 +0100

    Asset Classes Added

---
+++ ProjectJam/ProjectJam/Asset.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarmeladeAssets
+{
+    class Asset
+    {
+        // Properties
+        private int AssetID { get; set; }
+        private string AssetName { get; set; }
+        private DateTime AssetPurchaseDate { get; set; }
+        private decimal AssetPurchacePrice { get; set; }
+        private decimal AssetScrapValue { get; set; }
+        private decimal AssetPostedValue { get; set; }
+        private int AssetLifeSpan { get; set; }
+        private bool IsOperative { get; set; }
+        private List<Decreciation> AssetDecreciationList { get; set; }
+        private List<Maintenance> AssetMaintenanceList { get; set; }
+        
+        // Constructors:
+        public Asset()
+        {
+            this.AssetID = 1;// Get autoincrement from DB
+            this.AssetName = "Not Applied";
+            this.AssetPurchaseDate = DateTime.Today;
+            this.AssetPurchacePrice = 1000;
+            this.AssetScrapValue = 0;
+            this.AssetPostedValue = this.AssetPurchacePrice - this.AssetScrapValue;
+            this.AssetLifeSpan = 1;
+            this.IsOperative = false;
+            this.AssetDecreciationList = new List<Decreciation>();
+            this.AssetMaintenanceList = new List<Maintenance>();
+        }
+
+        // Methods:
+        public void CalculateDecreciations()
+        {
+            // DO STUFF
+        }
+
+    }
+}
---
+++ ProjectJam/ProjectJam/AssetController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarmeladeAssets
+{
+    class AssetController
+    {
+        //Properties:
+        private Asset CurrentAsset { get; set; }
+
+        // Methods:
+        public void InsertNewAsset()
+        {
+            // DO STUFF
+        }
+        public void ShowAssetInfo()
+        {
+            // DO STUFF
+        }
+        public void PlanMaintenance()
+        {
+            // DO STUFF
+        }
+        public void MaintenanceDone()
+        {
+            // DO STUFF
+        }
+        //public void PerformMaintenance()
+        //{
+        //    // DO STUFF
+        //}
+    }
+}
