426dcc43a58f21701f8c66ed20eebb604a943b2f
NPC <nick_paw@hotmail.com>
2016-02-10 14:12:07 +0100

    asset inventory på begyndt samt make asset

---
+++ ProjectJam/ProjectJam/AssetInventory.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectJam
+{
+    /// <summary>
+    /// Interaction logic for AssetInventory.xaml
+    /// </summary>
+    public partial class AssetInventory : Window
+    {
+        public AssetInventory()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void planMaintenanceButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void newAssetButton_Click(object sender, RoutedEventArgs e)
+        {
+            AssetMakeAsset newAssetWindow = new AssetMakeAsset();
+
+            newAssetWindow.Show();
+
+            this.Close();
+        }
+
+        private void assetInformationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void completedMaintenanceButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
