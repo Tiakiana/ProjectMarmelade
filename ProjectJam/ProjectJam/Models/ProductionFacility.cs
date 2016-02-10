10438adad65382e79704cf8401b25d51178a5947
EnchMan <sammyextra@gmail.com>
2016-02-10 13:55:45 +0100

    New Classes - Sam

---
+++ ProjectJam/ProjectJam/Models/ProductionFacility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectJam.Models
+{
+    class ProductionFacility
+    {
+        public enum FacilityStatus { Good, Working, Broken }
+
+        public FacilityStatus Status { get; set; }
+
+        public void CheckFacility()
+        {
+
+        }
+    }
+}
---
+++ ProjectJam/ProjectJam/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectJam.Models
+{
+    class Recipe
+    {
+        public int Id { get; private set; }
+        public List<Resource> Ingredients { get; set; }
+
+        public string Judgement { get; set; }
+    }
+}
