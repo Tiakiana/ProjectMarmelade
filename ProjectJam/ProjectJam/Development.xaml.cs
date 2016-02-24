f788a25829c52c914474b55403b4ea7afc7becde
kink <anderskink@gmail.com>
2016-02-24 16:29:14 +0100

    hitlerjugned. Anders . UI update

--- ProjectJam/ProjectJam/Development.xaml.cs
+++ ProjectJam/ProjectJam/Development.xaml.cs
@@ -19,9 +19,43 @@ namespace ProjectJam
     /// </summary>
     public partial class Development : Window
     {
-        public Development()
+        CreateRecipe cr = new CreateRecipe();
+        JudgeRecipe jr = new JudgeRecipe();
+        GetRecipe gr = new GetRecipe();
+        KnowlegdeBank kb = new KnowlegdeBank();
+        DeleteRecipe dr = new DeleteRecipe();
+        public void MainWindow()
         {
             InitializeComponent();
+            Domain.DomainController.getDCT().onStart("M1");
+
         }
+        /*
+        int prodID, DateTime date, 
+                string qualityTestActivities, string expresults, string results,
+                string comments, string comment, string employee)
+    */
+        #region UIRecipe
+        private void CreateRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RecipeFrame.Content = cr;
+        }
+        private void JudgeRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RecipeFrame.Content = jr;
+        }
+        private void GetRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RecipeFrame.Content = gr;
+        }
+        private void BankBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RecipeFrame.Content = kb;
+        }
+        private void DeleteRecipeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RecipeFrame.Content = dr;
+        }
+        #endregion
     }
 }
